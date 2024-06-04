d92cda1fd28034601ee4093d56216056fb6c1c86
fkeishi <166367547+fkeishi@users.noreply.github.com>
2024-06-04 18:56:06 +0900

    up

--- FAIRY_GUADIAN/Assets/Scenes/BarrierDirector.cs
+++ FAIRY_GUADIAN/Assets/Scenes/BarrierDirector.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 
 public class BarrierDirector : MonoBehaviour
 {
+    // バリアの耐久値
+    private int hp = 15;
+    // 一度ダメージを食らったら一定時間ダメージを食らわないようにする
+    private float damageCoolTime = 0f;
+    // ダメージ量
+    private int damage = 1;
+
+    bool enemyHit = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +24,16 @@ public class BarrierDirector : MonoBehaviour
     void Update()
     {
         
+
+        // 耐久値がなくなったら
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Damage()
+    {
+        hp--;
     }
 }
