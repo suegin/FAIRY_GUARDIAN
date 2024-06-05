3c97f050720f3d3f45094457ae71348f3bc367cb
nozabou <166367304+nozabou@users.noreply.github.com>
2024-06-05 18:55:44 +0900

    up

--- FAIRY_GUADIAN/Assets/Enemy.cs
+++ FAIRY_GUADIAN/Assets/Enemy.cs
@@ -2,66 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy1 : MonoBehaviour
 {
-    //// 敵プレハブ
-    //public GameObject Enemy1Prefab;
-    //// 時間間隔の最小値
-    //public float minTime = 2f;
-    //// 時間間隔の最大値
-    //public float maxTime = 5f;
-    //// 敵生成時間間隔
-    //private float interval;
-    //// 経過時間
-    //private float time = 0f;
-    GameObject Player;
 
+    GameObject Fairy;
     float speed = 0.005f;
-    
     // Start is called before the first frame update
     void Start()
     {
-        
         // Fairyを追跡
-        Player = GameObject.Find("Player");       
+        Fairy = GameObject.Find("Fairy");
     }
 
     // Update is called once per frame
     void Update()
     {
-        //// 時間計測
-        //time += Time.deltaTime;
-
-        //// 経過時間が生成時間になったとき(生成時間より大きくなった時)
-        //if (time > interval)
-        //{
-        //    // Enemyをインスタンス化する(生成する)
-        //    GameObject Enemy1 = Instantiate(Enemy1Prefab);
-        //    // 生成した敵の座標を決定する
-        //    Enemy1.transform.position = new Vector3(-4.61f, -0.49f, 0);
-        //    // 経過時間を初期化して再度時間計測を始める
-        //    time = 0f;
-        //    // 次に発生する時間間隔を決定する
-        //    // interval = GetRandomTime();
-        //}
-
-
 
         // EnemyがFairyを追跡する
-        float b = Player.transform.position.x - transform.position.x;
-        float c = (Player.transform.position.y - transform.position.y);
-            
-        float dis = (b * b + c *c );
-        float a=Mathf.Sqrt(dis);
-        float x = Player.transform.position.x - transform.position.x;
-        x= x/ a* speed;
-        float y = Player.transform.position.y - transform.position.y;
-        y = y / a* speed;
+        float b = Fairy.transform.position.x - transform.position.x;
+        float c = (Fairy.transform.position.y - transform.position.y);
+
+        float dis = (b * b + c * c);
+        float a = Mathf.Sqrt(dis);
+        float x = Fairy.transform.position.x - transform.position.x;
+        x = x / a * speed;
+        float y = Fairy.transform.position.y - transform.position.y;
+        y = y / a * speed;
         transform.position += new Vector3(x, y, transform.position.z);
 
-        Debug.Log(Player.transform.position);
-
-     
+        Debug.Log(Fairy.transform.position);
     }
-
 }
--- FAIRY_GUADIAN/Assets/EnemyHp.cs
+++ FAIRY_GUADIAN/Assets/EnemyHp.cs
@@ -4,44 +4,20 @@ using UnityEngine;
 
 public class EnemyHp : MonoBehaviour
 {
-    private int MaxHP
-    {
-        get
-        {
-            string tagName = this.gameObject.tag;
-
-            if (tagName == "Enemy1")
-            {
-                return 3;
-            }
-            else if (tagName == "Enemy2")
-            {
-                return 6;
-            }
-            else if (tagName == "Enemy3")
-            {
-                return 10;
-            }
-            else if (tagName == "Enemy4")
-            {
-                return 15;
-            }
-            else
-            {
-                return 5;
-            }
-        }
-    }
+    int Hp = 5;
     // Start is called before the first frame update
     void Start()
     {
-        print(this.gameObject.tag);
-        print(MaxHP);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if (Hp < 0)
+        {
+            this.gameObject.SetActive(false);
+        }
     }
 }
