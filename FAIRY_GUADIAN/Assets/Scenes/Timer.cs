0063d11dc5b4808a62bc4daf0a2f108649a3f826
suegin <166367284+suegin@users.noreply.github.com>
2024-06-12 18:45:39 +0900

    up

--- FAIRY_GUADIAN/Assets/Scenes/Timer.cs
+++ FAIRY_GUADIAN/Assets/Scenes/Timer.cs
@@ -1,13 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(Text))]
 public class ShowCountdown : MonoBehaviour
 {
-    public const float m_fStartTime = 300;
+    public const float m_fStartTime = 10;
     public string m_strFormat;
     public GameTimer m_gameTimer;
 
@@ -19,11 +17,15 @@ public class ShowCountdown : MonoBehaviour
         m_gameTimer = GameObject.Find("GameTimer").GetComponent<GameTimer>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         int fShowTime_Second = (int)(m_fStartTime - m_gameTimer.CurrentTime) % 60;
         int fShowTime_Minits = (int)(m_fStartTime - m_gameTimer.CurrentTime) / 60;
 
         m_txt.text = string.Format(m_strFormat, fShowTime_Minits.ToString() + ":" + fShowTime_Second.ToString());
+        if (fShowTime_Minits <= 0 && fShowTime_Second <= 0)
+        {
+            SceneManager.LoadScene("Clear Scene");
+        }
     }
 }
