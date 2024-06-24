a9a7e3214582d642c392426fe75c560a5fbc1ebb
suegin <166367284+suegin@users.noreply.github.com>
2024-06-24 18:38:22 +0900

    up

---
+++ FAIRY_GUADIAN/Assets/Clear Dilector.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClearDilector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("StageSelectScene");
+        }
+    }
+}
