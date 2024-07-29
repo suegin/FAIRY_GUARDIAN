a3c0739a68574d4faa271b375e2c87eb92704e36
suegin <ginjiPG@gmail.com>
2024-07-29 17:01:44 +0900

    up

--- FAIRY_GUADIAN/Assets/BGMScript.cs
+++ FAIRY_GUADIAN/Assets/BGMScript.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class BGMScript : MonoBehaviour
 {
+    public bool DontDestroyEnabled = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (DontDestroyEnabled)
+        {
+            // Sceneを遷移してもオブジェクトが消えないようにする
+            DontDestroyOnLoad(this);
+        }
     }
 
     // Update is called once per frame
--- FAIRY_GUADIAN/Assets/Scenes/GameOverDirector.cs
+++ FAIRY_GUADIAN/Assets/Scenes/GameOverDirector.cs
@@ -1,14 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameOverDirector : MonoBehaviour
 {
+    ChangeColorRGBA4 ChangeColorRGBA4;
+    private Image FadeImage;
+    private bool _isLoadGame = false;
+    private bool _isLoadStage = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        FadeImage = GetComponent<Image>();
+        ChangeColorRGBA4 = GetComponent<ChangeColorRGBA4>();
     }
 
     // Update is called once per frame
@@ -16,11 +20,29 @@ public class GameOverDirector : MonoBehaviour
     {
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            SceneManager.LoadScene("StageSelectScene");
+            ChangeColorRGBA4.FadeoutOn();
+            ChangeColorRGBA4.Update();
+            _isLoadGame = true;
         }
         if (Input.GetKeyUp(KeyCode.Return))
         {
+            ChangeColorRGBA4.FadeoutOn();
+            ChangeColorRGBA4.Update();
+            _isLoadStage = true;
+        }
+    }
+
+    public void LoadNextScene()
+    {
+        if (_isLoadGame) 
+        {
+            _isLoadGame = false;
             SceneManager.LoadScene("GameScene");
+        }
+        else if (_isLoadStage)
+        {
+            _isLoadStage = false;
+            SceneManager.LoadScene("StageSelectScene");
         }
     }
 }
